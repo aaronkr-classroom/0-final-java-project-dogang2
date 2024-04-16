41171717c1b4766777be279c82792404dd0b716e
20211814김동연 <161808977+dogang2@users.noreply.github.com>
2024-04-16 20:48:48 +0900

    ! 끝끝끝

--- src/CartItem.java
+++ src/CartItem.java
@@ -1,13 +1,14 @@
 public class CartItem {
 	
-	private String[] itemBook = new String[7];
+	//private String[] itemBook = new String[7];
+	private Book itemBook;
 	private String bookID;
 	private int quantity;
 	private int totalPrice;
 	
 	public 	CartItem() {}
 	
-	public 	CartItem(String[] book) {
+	/*public 	CartItem(String[] book) {
 		this.itemBook = book;
 		this.bookID = book[0];
 		this.quantity = 1;
@@ -20,6 +21,26 @@ public class CartItem {
 	
 	public String[] getItemBook(String[] itembook) {
 		return itembook = itemBook;
+	}
+*/	
+	
+	public CartItem(Book booklist) {
+		this.itemBook = booklist;
+		this.bookID = booklist.getBookId();
+		this.quantity = 1;
+		updateTotalPrice();
+	}
+	
+	public Book getItemBook() {
+		return itemBook;
+	}
+	
+	public void setItemBook(Book itemBook) {
+		this.itemBook = itemBook;
+	}
+	
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
 	}
 	
 	public String getBookID() {
@@ -43,6 +64,7 @@ public class CartItem {
 	}
 	
 	public void updateTotalPrice() {
-		totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
+		//totalPrice = Integer.parseInt(this.itemBook[2]) * this.quantity;
+		totalPrice = this.itemBook.getUnitPrice() * this.quantity;
 	}
 }
